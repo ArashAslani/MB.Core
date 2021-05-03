8af5d0b8dc77e78f6dd080d7cc95099f49b4d8a8
LashaOn <83548643+LashaOn@users.noreply.github.com>
2021-05-04 03:47:20 +0430

    The previous version of this project had some bugs fixed

--- MB.Infrastructure.Core/Bootstrapper.cs
+++ MB.Infrastructure.Core/Bootstrapper.cs
@@ -1,20 +1,23 @@
 ﻿using MB.Application;
 using MB.Application.Contracts.ArticleCategory;
 using MB.Domain.ArticleCategoryAgg;
+using MB.Domain.ArticleCategoryAgg.Services;
 using MB.Infrastructure.EfCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace MB.Infrastructure.Core
 {
-    public static class Bootstraper
+    public static class Bootstrapper
     {
-        public static void Config(IServiceCollection services , string conectionString)
+        public static void Config(IServiceCollection services , string connectionString)
         {
+            services.AddDbContext<MBContext>(option => option.UseSqlServer(connectionString));
+            services.AddTransient<IArticleCategoryValidatorService, ArticleCategoryValidatorService>();
             services.AddTransient<IArticleCategoryApplication , ArticleCategoryApplication>();
             services.AddTransient<IArticleCategoryRepository, ArticleCategoryRepository>();
-            services.AddDbContext<MBContext>(option => option.UseSqlServer(conectionString));
-
+          
         }
     }
 }
+ 
